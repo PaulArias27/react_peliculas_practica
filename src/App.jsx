eee19da903ab9ac0b4a4e591d4c0330cdc6deeda
PaulArias27 <paularias2727@gmail.com>
2026-06-28 00:26:39 -0500

    PA: se implemento estado con useState

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,24 @@
-
+import { useState } from 'react';
 import './App.css'
+
 
 function Pelicula({titulo}){
 
+  const [esFavorita, setEsFavorita] = useState(false);
+
   return (
-    <div>
-      <h3>{titulo}</h3>
+    <div onClick={() => setEsFavorita(!esFavorita) }
+        style={{
+        border: "1px solid gray",
+        padding: "10px",
+        margin: "8px 0",
+        borderRadius: "8px",
+        cursor: "pointer",
+        backgroundColor: esFavorita ? "#fff8b3" : "#f5f5f5",
+        fontWeight: esFavorita ? "bold" : "normal",
+      }}>
+
+      <h3>{titulo} {esFavorita ? "⭐" : "" }</h3>
     </div>
   );
 }
