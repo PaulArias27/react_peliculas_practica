ac047886e0f809d64cd0a53994ac94a4f420cae3
PaulArias27 <paularias2727@gmail.com>
2026-06-27 23:56:47 -0500

    PA: se creo el componente pelicula, recibe prop y muestra en pantalla

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,25 @@
 
 import './App.css'
 
+function Pelicula({titulo}){
+
+  return (
+    <div>
+      <h3>{titulo}</h3>
+    </div>
+  );
+}
+
 function App() {
  
 
   return (
-    <h1>Mis Películas Favoritas</h1>
+    <div>
+      <h1>Mis Películas Favoritas</h1>
+    <Pelicula titulo='Interestelar'/>
+    <Pelicula titulo='Batman'/>
+
+    </div>
   )
 }
 
